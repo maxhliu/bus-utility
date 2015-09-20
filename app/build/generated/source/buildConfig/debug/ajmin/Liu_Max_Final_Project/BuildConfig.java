2e79915d9b3a9da9cc67201abd7b08fd12db78b6
Max Liu <lium001@gmail.com>
2015-09-20 18:30:14 -0400

    First Commit

---
+++ app/build/generated/source/buildConfig/debug/ajmin/Liu_Max_Final_Project/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ajmin.Liu_Max_Final_Project;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ajmin.Liu_Max_Final_Project";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
